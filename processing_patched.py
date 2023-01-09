23943af37620317bef589f2a1da2fdeede3d8fef
Mohammad Farahmand <mohammedfarahmand98@gmail.com>
2023-01-09 11:41:02 +0330

    Added a new file naming mechanism.

--- processing_patched.py
+++ processing_patched.py
@@ -23,6 +23,7 @@ class Video:
         self.source_dir = source_dir
         self.target_dir = target_dir
         self.destination = destination
+        self.relative_frame_id = args.relative_frame_id
         self.load_video(source_dir / self.video_filename)
         self.load_bounds(source_dir / self.bounds_filename)
         self.load_annotations(source_dir / self.annotations_filename)
@@ -78,6 +79,7 @@ class Video:
             info.write(stream)
 
     def extract_frames(self, video_id, image_id_offset):
+        self.frame_filenames = {}
         self.image_ids = {}
         frames = []
 
@@ -87,10 +89,11 @@ class Video:
         with tqdm.tqdm(total=self.num_frames, desc='* Frames', leave=False) as progress:
             read, frame = self.capture.read()
             frame_count = 1
-            frame_id = 1
+            frame_index = 1
             while read:
-                if (frame_id - 1) % (self.args.skip_frames + 1) == 0:
-                    output_filepath = output_dir / f'{frame_id:06d}{self.images_extension}'
+                if (frame_index - 1) % (self.args.skip_frames + 1) == 0:
+                    frame_filename = frame_count if self.relative_frame_id else frame_index
+                    output_filepath = output_dir / f'{frame_filename:06d}{self.images_extension}'
                     cv2.imwrite(str(output_filepath), frame, [cv2.IMWRITE_JPEG_QUALITY, self.args.quality])
                     image_id = image_id_offset + frame_count
                     di = {'id': image_id,
@@ -98,13 +101,14 @@ class Video:
                           'frame_id': frame_count,
                           'file_name': str(output_filepath.relative_to(self.target_dir.parent)),
                           'width': self.p_width, 'height': self.p_height}
-                    self.image_ids[frame_id] = image_id
+                    self.frame_filenames[frame_index] = frame_filename
+                    self.image_ids[frame_index] = image_id
                     frames.append(di)
                     frame_count += 1
-                frame_id += 1
+                frame_index += 1
                 progress.update(1)
                 read, frame = self.capture.read()
-        assert frame_id > 1, 'Failed to read the frames.'
+        assert frame_index > 1, 'Failed to read the frames.'
 
         return frames
 
@@ -126,8 +130,8 @@ class Video:
                         for child in element:
                             if child.tag == 'box':
                                 attributes = child.attrib
-                                frame_id = int(attributes['frame']) + self.annotations_offset
-                                if frame_id in self.image_ids:
+                                frame_index = int(attributes['frame']) + self.annotations_offset
+                                if frame_index in self.image_ids:
                                     left, top = float(attributes['xtl']), float(attributes['ytl'])
                                     right, bottom = float(attributes['xbr']), float(attributes['ybr'])
                                     assert right > left and bottom > top, 'Invalid Bounding Box'
@@ -145,11 +149,12 @@ class Video:
                                     bounding_box = tuple(bounding_box)
                                     area = round(width * height)
                                     rest = 1, 0, 0, 0  # Confidence, 3D Coordinates
-                                    line = frame_id, track_id, *bounding_box, *rest
+                                    frame_filename = self.frame_filenames[frame_index] if self.relative_frame_id else frame_index
+                                    line = frame_filename, track_id, *bounding_box, *rest
                                     line = ','.join(str(el) for el in line)
                                     stream.write(f'{line}\n')
                                     di = {'id': box_id_offset + box_id,
-                                          'image_id': self.image_ids[frame_id],
+                                          'image_id': self.image_ids[frame_index],
                                           'track_id': track_id_offset + track_id,
                                           'bbox': bounding_box,
                                           'area': area,
@@ -170,6 +175,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--data-dir', default='dataset', help='path to directory containing raw_data')
     parser.add_argument('--skip-frames', type=int, default=3, help='number of frames to skip between images')
+    parser.add_argument('--relative-frame-id', action='store_true', help='when set, filenames will be relative frame ids')
     parser.add_argument('--quality', type=int, default=100, help='level of quality when saving JPEG images')
     parser.add_argument('--patch-size', nargs=2, type=int, default=(640, 480), help='desired patch size')
     args = parser.parse_args()
